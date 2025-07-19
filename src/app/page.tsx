0d99ebb9f40ab824d09b998a57c54288a1ecf47d
Ilyas Ayachi <hi@ilyasayachi.com>
2025-07-19 17:01:45 +0200

    fix: import and use missing components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import ProfileSection from "@/components/profile-section";
 import AboutSection from "@/components/about-section";
+import ExperienceSection from "@/components/experience-section";
 
 export default function Home() {
   return (
@@ -8,7 +9,8 @@ export default function Home() {
         <ProfileSection />
       </div>
       <div className="flex flex-col gap-8 max-w-3xl w-full">
-
+        <AboutSection />
+        <ExperienceSection />
       </div>
     </div>
   );
