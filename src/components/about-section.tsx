4c8023f179085e627ff1c56486e3d0a2a1c3be0e
Ilyas Ayachi <hi@ilyasayachi.com>
2025-07-19 17:16:06 +0200

    fix: resolve ESLint no-unescaped-entities error

--- src/components/about-section.tsx
+++ src/components/about-section.tsx
@@ -3,12 +3,13 @@ export default function AboutSection() {
     <div className="flex flex-col gap-4 font-medium">
       <h2>About</h2>
       <p className="text-sm">
-        I'm a passionate self-taught software developer, proudly Franco-Belgian,
-        with a deep love for building innovative, scalable, and user-friendly
-        applications. I started coding as a kid, tinkering with my computer to
-        figure out how the applications I used every day actually worked. Since
-        then, I’ve explored a wide range of technologies, from frontend to
-        backend. Today, software development is at the heart of everything I do.
+        I&apos;m a passionate self-taught software developer, proudly
+        Franco-Belgian, with a deep love for building innovative, scalable, and
+        user-friendly applications. I started coding as a kid, tinkering with my
+        computer to figure out how the applications I used every day actually
+        worked. Since then, I&apos;ve explored a wide range of technologies,
+        from frontend to backend. Today, software development is at the heart of
+        everything I do.
       </p>
     </div>
   );
